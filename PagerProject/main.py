8bf10d903f2367ba233e82586e478479dba5f12a
ejheid <134549848+ejheid@users.noreply.github.com>
2023-05-25 15:25:30 -0600

    Update main.py now retrieving country codes

--- PagerProject/main.py
+++ PagerProject/main.py
@@ -2,51 +2,36 @@ import scipy.spatial
 import XmlToGrid
 import cv2
 import numpy as np
+import PIL
+from PIL import Image
 
 grid = XmlToGrid.ShakeMapGrid()
 grid.load("us20005j32.xml")
-print(len(grid.grid))
-print(grid.grid[0]["LON"])
-
-
-
-
-
-
 
 
 # Read the tif file
-image = cv2.imread('countriesISOraster.tif')
-print(len(image))
+PIL.Image.MAX_IMAGE_PIXELS = 900115200
+img = Image.open('countriesISOraster.tif')
+img_arr = np.array(img)
 
 range_of_lon=np.arange(-180.0000,179.999986,0.0083334)
 range_of_lat=np.arange(83.6333,-90.0000,-0.0083334)
 
-print("lat then lon range:", len(range_of_lat), len(range_of_lon))
-
-lat_vals = [grid.lat_min,grid.lat_max, grid.lat]
-lon_vals = [grid.lon_min,grid.lon_max, grid.lon]
-
-lat_corner=[grid.lat_min,grid.lat_max,grid.lat_min,grid.lat_max]
-lon_corner=[grid.lon_max,grid.lon_max,grid.lon_min,grid.lon_min]
-corners = [lat_corner, lon_corner]
-print(corners)
+lon_vals=[grid.lon_min,grid.lon_max,grid.lon_min,grid.lon_max, grid.lon]
+lat_vals=[grid.lat_max,grid.lat_max,grid.lat_min,grid.lat_min, grid.lat]
 col = []
 row = []
+
+for x in lon_vals:
+    curr_idx = ((np.abs(range_of_lon - x)).argmin())
+    col.append(curr_idx)
 
 for x in lat_vals:
     curr_idx = ((np.abs(range_of_lat - x)).argmin())
     row.append(curr_idx)
 
-for x in lon_vals:
-    curr_idx = ((np.abs(range_of_lon - x)).argmin())
-    col.append(curr_idx)
-print("cols: ", col)
-print("rows: ", row)
 
-print(grid.lon)#how to get epicenter
-print(grid.lat)#how to get epicenter
-
-
-print(grid.lat_max)#coorners
-print(grid.lat_min)#
+corners = [row, col]
+print(corners)
+ccode = img_arr[row,col]
+print(ccode)
