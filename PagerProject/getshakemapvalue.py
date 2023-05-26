b0f878e4bede8f41888735d3ef00f188d32ed70e
gazim6645 <134646346+gazim6645@users.noreply.github.com>
2023-05-26 12:27:51 -0600

    Add files via upload

---
+++ PagerProject/getshakemapvalue.py
@@ -0,0 +1,11 @@
+
+
+def getshakemapvalue(shakeoutput, Exposure):
+    expo_lat=Exposure["LATITUDE"]
+    expo_lon=Exposure["LONGITUDE"]
+
+    #Have to figure out how we can we get only the column data for the xml file. It seems challenging
+    #shake_coords = shakeoutput.data(:,1:2);  
+    #shake_gm = shakeoutput.data(:,3:end);
+
+
