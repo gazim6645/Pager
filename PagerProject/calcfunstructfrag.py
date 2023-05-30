a6f25887a38c77bfba420199d4bf31c7b94ec44c
ejheid <134549848+ejheid@users.noreply.github.com>
2023-05-30 14:33:26 -0600

    Update calcfunstructfrag.py

--- PagerProject/calcfunstructfrag.py
+++ PagerProject/calcfunstructfrag.py
@@ -1,17 +1,32 @@
+import numpy as np
+from ismember import ismember
+
 
 def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
-    unique_sm_tax = set(sm_expo['TAXONOMY'])
+    unique_sm_tax =set(sm_expo['TAXONOMY'])
     #print(unique_sm_tax)
 
-    damage_states = set(structfrag['Damage_state'])
+    damage_states =set(structfrag['Damage_state'])
+
     
+    tax_idx = np.array(taxonomymap.loc[taxonomymap['taxonomy'].isin(unique_sm_tax)].index)
+
     for current_damage_states in damage_states:
         structfrag_sub=structfrag[structfrag['Damage_state']==current_damage_states] #structfrag_sub = structfrag(structfrag.Damage_state==damage_states(j),:); This is done
-        print(structfrag_sub)
-        
 
 
-
+        print(len(structfrag_sub['Building_class']))
+        vuln_idx = np.array(structfrag_sub.loc[~structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])].index)
+        print(vuln_idx)
+        print(len(vuln_idx))
+    
+    
+    
+    
+    
+    
+    
+    
     #return result, damage_states
     
     return 0, 0
