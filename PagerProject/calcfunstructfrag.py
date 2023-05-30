ac8857627d7c98575c5d53d34090cee51b312722
ejheid <134549848+ejheid@users.noreply.github.com>
2023-05-30 16:22:35 -0600

    Update calcfunstructfrag.py

--- PagerProject/calcfunstructfrag.py
+++ PagerProject/calcfunstructfrag.py
@@ -3,36 +3,39 @@ from ismember import ismember
 
 
 def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
-    unique_sm_tax =set(sm_expo['TAXONOMY'])
+    unique_sm_tax =sm_expo['TAXONOMY']
     #print(unique_sm_tax)
 
     damage_states =set(structfrag['Damage_state'])
 
     
     tax_idx = np.array(taxonomymap.loc[taxonomymap['taxonomy'].isin(unique_sm_tax)].index)
-
+    
     for current_damage_states in damage_states:
+        
         structfrag_sub=structfrag[structfrag['Damage_state']==current_damage_states] #structfrag_sub = structfrag(structfrag.Damage_state==damage_states(j),:); This is done
 
         structfrag_sub=structfrag_sub.reset_index()
         del structfrag_sub['index']
 
+        vuln_idx=np.array(structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])].index)
         
-        
-        varOne=structfrag_sub['Building_class']
-        varTwo=taxonomymap['conversion'][tax_idx]
+        '''
+        a=find(tax_idx==0);
+        b=find(vuln_idx==0);
+        if length(a)>=1 || length(b)>=1
+            
+        pause;
+        end
+        '''
 
-
-        #vuln_idx = np.array(varTwo.isin(varOne))
-        vuln_idx=np.isin(varTwo, varOne)
-        print(len((vuln_idx)))
-        print(vuln_idx)
+        print(taxonomymap['taxonomy'][tax_idx])
+        print(structfrag_sub.iloc[vuln_idx])
+        vuln_table = taxonomymap['taxonomy'][tax_idx].join(structfrag_sub.iloc[vuln_idx], how='right', sort=True);
+        print(vuln_table)
+    
+    
         break
-
-        
-    
-    
-    
     
     
     
