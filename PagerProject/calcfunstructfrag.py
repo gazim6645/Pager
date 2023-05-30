503aa7d81ebc004547e4c8d21f566334a46ec8f6
gazim6645 <134646346+gazim6645@users.noreply.github.com>
2023-05-30 12:29:54 -0600

    Add files via upload

---
+++ PagerProject/calcfunstructfrag.py
@@ -0,0 +1,11 @@
+
+def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
+    unique_sm_tax = set(sm_expo['TAXONOMY'])
+    #print(unique_sm_tax)
+
+    damage_states = set(structfrag['Damage_state'])
+    print(damage_states)
+
+
+    #return result, damage_states
+    return 0, 0
