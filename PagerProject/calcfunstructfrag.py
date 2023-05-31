5aa12c153170fe758a8654385b44f79dc97ab4e4
gazim6645 <134646346+gazim6645@users.noreply.github.com>
2023-05-31 14:45:39 -0600

    Update calcfunstructfrag.py

--- PagerProject/calcfunstructfrag.py
+++ PagerProject/calcfunstructfrag.py
@@ -1,15 +1,15 @@
 import numpy as np
-from ismember import ismember
+import pandas as pd
 
 
 def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
-    unique_sm_tax =sm_expo['TAXONOMY']
-    #print(unique_sm_tax)
-
+    unique_sm_tax =set(sm_expo['TAXONOMY'])
+    
     damage_states =set(structfrag['Damage_state'])
 
     
-    tax_idx = np.array(taxonomymap.loc[taxonomymap['taxonomy'].isin(unique_sm_tax)].index)
+    tax_idx = set(np.array(taxonomymap.loc[taxonomymap['taxonomy'].isin(unique_sm_tax)].index))
+    
     
     for current_damage_states in damage_states:
         
@@ -18,8 +18,22 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
         structfrag_sub=structfrag_sub.reset_index()
         del structfrag_sub['index']
 
-        vuln_idx=np.array(structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])].index)
         
+       
+
+        #vuln_idx=np.array(structfrag_sub.loc[taxonomymap['conversion'][tax_idx].isin(structfrag_sub['Building_class'])])
+        #vuln_idx=structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])].index
+        #, drop_duplicates=False)
+        #vuln_idx=structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx]), keep_duplicates=False].index
+        #vuln_idx=structfrag_sub[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])]
+        #print(len(taxonomymap['conversion'][tax_idx]))
+        #print(len(taxonomymap['conversion'][tax_idx].unique()))
+        vuln_idx = []
+        for i in taxonomymap['conversion'][tax_idx]:
+            curr_idx = structfrag_sub.loc[structfrag_sub['Building_class'] == i].index
+            vuln_idx.append(curr_idx[0])
+        
+
         '''
         a=find(tax_idx==0);
         b=find(vuln_idx==0);
@@ -28,15 +42,20 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
         pause;
         end
         '''
-
-        print(taxonomymap['taxonomy'][tax_idx])
-        print(structfrag_sub.iloc[vuln_idx])
-        vuln_table = taxonomymap['taxonomy'][tax_idx].join(structfrag_sub.iloc[vuln_idx], how='right', sort=True);
+        df1 = pd.DataFrame(structfrag_sub.iloc[vuln_idx]).reset_index()
+        del df1['index']
+        df2 = pd.DataFrame(taxonomymap['taxonomy'][tax_idx]).reset_index()
+        del df2['index']
+        vuln_table = df2.join(df1, how='right', sort=True)
         print(vuln_table)
-    
-    
-        break
-    
+
+    '''
+    if istable(countrystructfrag)
+        countrystructfrag_sub=countrystructfrag(countrystructfrag.Damage_state(j),:);
+        [~,iglobal,icountry] = intersect(vuln_table.Building_class,countrystructfrag_sub.Building_class);
+        vuln_table(iglobal,:) = [vuln_table(iglobal,"taxonomy") countrystructfrag_sub(icountry,:)];
+    end
+    '''
     
     
     
