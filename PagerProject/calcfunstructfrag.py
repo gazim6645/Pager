a2eddaf5fa86d68da1bb255f709731fd53baa539
ejheid <134549848+ejheid@users.noreply.github.com>
2023-06-06 14:11:52 -0600

    Update calcfunstructfrag.py

--- PagerProject/calcfunstructfrag.py
+++ PagerProject/calcfunstructfrag.py
@@ -15,6 +15,8 @@ from scipy.interpolate import RegularGridInterpolator
 imls = [0.05,0.0562,0.0631,	0.0709,	0.0796,	0.0895,	0.1005,	0.1129,	0.1269,	0.1425,	0.1601,	0.1799	,0.2021,0.2271,0.2551,0.2866,0.322,0.3617,0.4064,0.4565,0.5129,	0.5762,	0.6474,	0.7273,	0.8171,	0.9179,	1.0312,	1.1585	,1.3016	,1.4622	,1.6428	,1.8456,2.0734,	2.3294,	2.6169,	2.94,3.3029,3.7106,	4.1687,	4.6833,	5.2615,	5.911,6.6407,7.4605,8.3815,9.4162,10.5787,11.8846,13.3517,15]
 
 def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
+
+    
     
     unique_sm_tax =set(sm_expo['TAXONOMY'])
     
@@ -26,27 +28,14 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
     
     
     for current_damage_states in damage_states:
-        #current_damage_states="slight"
 
-        
+
         structfrag_sub=structfrag[structfrag['Damage_state']==current_damage_states] #structfrag_sub = structfrag(structfrag.Damage_state==damage_states(j),:); This is done
 
         structfrag_sub=structfrag_sub.reset_index()
         del structfrag_sub['index']
 
         
-       
-
-        #vuln_idx=np.array(structfrag_sub.loc[taxonomymap['conversion'][tax_idx].isin(structfrag_sub['Building_class'])])
-        #vuln_idx=structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])].index
-        #, drop_duplicates=False)
-        #vuln_idx=structfrag_sub.loc[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx]), keep_duplicates=False].index
-        #vuln_idx=structfrag_sub[structfrag_sub['Building_class'].isin(taxonomymap['conversion'][tax_idx])]
-        #print(len(taxonomymap['conversion'][tax_idx]))
-        #print(len(taxonomymap['conversion'][tax_idx].unique()))
-        
-        
-
         # from matlab code seems unnecessary
         '''
         a=find(tax_idx==0);
@@ -71,23 +60,6 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
         del df2['index']
         vuln_table = df2.join(df1, how='right', sort=True)
 
-        
-        '''
-        # improved way of combining taxonomy map with structfrag_sub
-        df1 = pd.DataFrame(taxonomymap['conversion'][tax_idx]).reset_index() 
-        del df1['index']
-
-        structfrag_sub = structfrag_sub.rename(columns={'Building_class': 'conversion'})
-        vuln_table = pd.merge(df1, structfrag_sub, how='inner', on=['conversion'])
-
-        df2 = pd.DataFrame(taxonomymap['taxonomy'][tax_idx]).reset_index()
-        del df2['index']
-        
-        vuln_table = df2.join(vuln_table, how='right', sort=True)
-        vuln_table = vuln_table.rename(columns={'conversion': 'Building_class', 'taxonomy': 'TAXONOMY'})
-        structfrag_sub = structfrag_sub.rename(columns={'conversion': 'Building_class'})
-        '''
-
         # again, seems unnecessary
         '''
         if istable(countrystructfrag)
@@ -104,16 +76,7 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
         #need to fix sm_vuln_table
         
         shake_input = [0]*len(sm_vuln_table)
-        """
-        val_pga = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="PGA"].index)
-        val_sa3 = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="SA(0.3s)"].index)
-        val_sa1 = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="SA(1.0s)"].index)
-      
-        pga=dict(zip_longest(val_pga, np.array(sm_expo['PGA'][val_pga]), fillvalue=None))
         
-        sa3=dict(zip_longest(val_sa3, np.array(sm_expo['PSA03'][val_sa3]), fillvalue=None))
-        
-        sa1=dict(zip_longest(val_sa1, np.array(sm_expo['PSA10'][val_sa1]), fillvalue=None))"""
         val_pga = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="PGA"].index)
         val_sa3 = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="SA(0.3s)"].index)
         val_sa1 = np.array(sm_vuln_table.loc[sm_vuln_table["IMT"]=="SA(1.0s)"].index)
@@ -135,18 +98,26 @@ def calcfunstructfrag(sm_expo, structfrag, taxonomymap,countrystructfrag):
         sk=sm_expo.loc[sm_expo["COST_STRUCTURAL_USD"].isna()].index
         
         if(len(sk)!=0):
-            
             sm_expo["COST_STRUCTURAL_USD"][sk]=0.7*sm_expo["TOTAL_REPL_COST_USD"][sk]
 
         tabV=(sm_vuln_table.iloc[:,4:]).to_numpy()
         SI=shake_input
 
-        cost_ratio=np.array([np.interp(SI[i], imls, tabV[i]) for i in range( tabV.shape[0])]) #np.array([np.interp(SI[i], imls, tabV[i]) for i in range(SI.size)])
-        
-        #cost_ratio[cost_ratio==cost_ratio[5849]] = 0
-
-        
+        #cost_ratio = interp1d(imls, tabV[i],  fill_value='extrapolate') 
+        cost_ratio = []
+        for i in range(SI.size):
+            f=interp1d(imls, tabV[i],  fill_value='extrapolate') 
+            curr = f(SI[i]).max()
+            if (curr < 0):
+                curr = 0
+            cost_ratio.append(curr)
+        '''
+        file1=open("myfile.txt","w")
+        for i in range(0,len(cost_ratio)):
+            file1.writelines(str(cost_ratio[i])+"\n")
+        file1.close()
+        '''
         result[current_damage_states]=cost_ratio*sm_expo["COST_STRUCTURAL_USD"]
-        
+
 
     return result, damage_states
