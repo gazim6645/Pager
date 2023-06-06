6b87e0b339a0225cefa57b03ded1c79293d67098
ejheid <134549848+ejheid@users.noreply.github.com>
2023-06-06 14:50:03 -0600

    Update calcfuncontentsexp.py

--- PagerProject/calcfuncontentsexp.py
+++ PagerProject/calcfuncontentsexp.py
@@ -60,14 +60,25 @@ def calcfuncontentsexp(sm_expo, contentsvuln, taxonomymap,countrycontentsvuln):
     SI=shake_input
 
     #cost_ratio=np.array([np.interp(SI[i], imls, tabV[i], left=0) for i in range(SI.size)]) 
-    
+    '''
     cost_ratio = []
     for i in range(SI.size):
         f=interp1d(imls, tabV[i],  fill_value='extrapolate') 
         curr = f(SI[i]).max()
+        if (curr < 0):
+            curr = 0
         cost_ratio.append(curr)
+    '''
+    cost_ratio=[]
+    for i in range(SI.size):
+        if(SI[i] <imls[0]):
+            cost_ratio.append((interp1d(imls, tabV[i],  fill_value='extrapolate'))(SI[i]))
+        else:
+            cost_ratio.append(np.interp(SI[i], imls, tabV[i]))
+
+
+    cost_ratio = np.array(cost_ratio)
     cost_ratio[cost_ratio < 0] = 0
-    
     result=cost_ratio*sm_expo["COST_CONTENTS_USD"]
     return result
 
